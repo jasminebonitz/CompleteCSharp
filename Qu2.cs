539dd56461db108b3a9615ce5b649bde14156056
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-10 16:43:51 -0700

    Add files via upload

---
+++ Qu2.cs
@@ -0,0 +1,34 @@
+﻿// Receives user input and displays it.
+
+using System;
+
+namespace Qu2
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string name, uni;
+			int age, year;
+
+			Console.WriteLine("Bitte geben Sie Ihren Namen ein.");
+			name = Console.ReadLine();
+
+			Console.WriteLine("\nBitte geben Sie Ihr Alter ein.");
+			age = Int32.Parse(Console.ReadLine());
+
+			Console.WriteLine("\nAuf welche Uni gehen Sie?");
+			uni = Console.ReadLine();
+
+			Console.WriteLine("\nIn welchem Jahr der Uni sind Sie?");
+			year = Int32.Parse(Console.ReadLine());
+
+			Console.WriteLine("\n------------------\n");
+			Console.WriteLine("Student/in Informationen:\n");
+			Console.WriteLine("Name : {0}\n", name);
+			Console.WriteLine("Alter : {0}\n", age);
+			Console.WriteLine("Uni : {0}\n", uni);
+			Console.WriteLine("Jahr : {0}", year);
+		}
+	}
+}
