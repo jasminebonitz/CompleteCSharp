cc99a1df68d29993e213cb01d83605f3f079809a
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-11 22:24:06 -0700

    Add files via upload

---
+++ EvenOrOdd.cs
@@ -0,0 +1,30 @@
+﻿// Checks whether the input number is even or odd.
+
+using System;
+
+namespace EvenOrOdd
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int num;
+
+		Start:
+			Console.WriteLine("Enter a number.");
+			num = Int32.Parse(Console.ReadLine());
+
+			if (num % 2 == 0)
+			{
+				Console.WriteLine("\n{0} is an even number.", num);
+				Console.WriteLine("-----------");
+			}
+			else
+			{
+				Console.WriteLine("\n{0} is an odd number.", num);
+				Console.WriteLine("-----------");
+			}
+			goto Start;
+		}
+	}
+}
