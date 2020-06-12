854f69104e9d14ff6ab28dbdb1e965e423d48d9b
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-11 21:50:08 -0700

    Add files via upload

---
+++ Formula.cs
@@ -0,0 +1,25 @@
+﻿// Calculates (a+b)^2 and returns the output.
+
+using System;
+
+namespace Formula
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int a, b, result;
+
+			Console.WriteLine("Enter a number.");
+			a = Int32.Parse(Console.ReadLine());
+
+			Console.WriteLine("\nEnter a second number.");
+			b = Int32.Parse(Console.ReadLine());
+
+			result = a + b;
+			result *= result;
+
+			Console.WriteLine("\n({0}+{1})^2 = {2}", a, b, result);
+		}
+	}
+}
