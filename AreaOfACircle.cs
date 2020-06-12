3419b8bf9d1f30ba7447371c97bc76cb1db06621
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-11 21:51:24 -0700

    Add files via upload

---
+++ AreaOfACircle.cs
@@ -0,0 +1,22 @@
+﻿// Calulates the area of a circle (πr^2) and returns the result.
+
+using System;
+
+namespace AreaOfACircle
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			double r, result;
+			double pi = Math.PI;
+
+			Console.WriteLine("Enter a radius (cm).");
+			r = Int32.Parse(Console.ReadLine());
+
+			result = pi * r * r;
+
+			Console.WriteLine("\nThe area of a circle with the given radius is {0}cm.", result);
+		}
+	}
+}
