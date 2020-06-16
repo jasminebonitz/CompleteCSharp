0d5a4d4e9d541d18cc5a3b6c60118dc252e30206
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-16 11:39:34 -0700

    Add files via upload

---
+++ ReturnStatementDemo.cs
@@ -0,0 +1,29 @@
+﻿// Returns values from mathematic operations within each class.
+
+using System;
+
+namespace ReturnStatementDemo
+{
+	public class Addition
+	{
+		public static int Test(int a, int b)
+		{
+			return a + b;
+		}
+	}
+	public class Subtraction
+	{
+		public static int Test(int a, int b)
+		{
+			return a - b;
+		}
+	}
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine(Addition.Test(5, 45));
+			Console.WriteLine(Subtraction.Test(2, 10));
+		}
+	}
+}
