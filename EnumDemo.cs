a286bc5a726405443277a29473644b27b3219a00
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-16 11:40:17 -0700

    Add files via upload

---
+++ EnumDemo.cs
@@ -0,0 +1,22 @@
+﻿// Displays an instance of enumeration.
+
+using System;
+
+namespace EnumDemo
+{
+	enum colors
+	{
+		red,
+		yellow,
+		green
+	}
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("The numeric position of red is " + (int)colors.red);
+			Console.WriteLine("The numeric position of yellow is " + (int)colors.yellow);
+			Console.WriteLine("The numeric position of green is " + (int)colors.green);
+		}
+	}
+}
