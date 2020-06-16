2e2e8aafa37dcb717cdb24f8e7dcb9734a45a5d7
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-16 11:39:56 -0700

    Add files via upload

---
+++ ContinueBreakDemo.cs
@@ -0,0 +1,25 @@
+﻿// Demostrates continue/break usage.
+
+using System;
+
+namespace BreakContinueDemo
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			for (int i = 0; i <= 20; i++)
+			{
+				if (i == 0)             // Skips 0 and continues the executing the loop.
+				{
+					continue;
+				}
+				else if (i == 11)       // Stops iterating through the loop when it reaches 11.
+				{
+					break;
+				}
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
