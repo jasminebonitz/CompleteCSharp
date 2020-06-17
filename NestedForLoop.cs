0c255ff3f40e455f8e74e99686d2d3587e6c773b
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-17 10:56:02 -0700

    Add files via upload

---
+++ NestedForLoop.cs
@@ -0,0 +1,31 @@
+﻿// Outputs a triangle of ascending and descending numbers.
+
+using System;
+
+namespace NestedForLoop
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int i, j, m, n;
+
+			for (i = 1; i <= 5; i++)
+			{
+				for (j = i; j < 5; j++)
+				{
+					Console.Write(" ");
+				}
+				for (m = 1; m < i; m++)
+				{
+					Console.Write(m);
+				}
+				for (n = i; n > 0; n--)
+				{
+					Console.Write(n);
+				}
+				Console.WriteLine("\n");
+			}
+		}
+	}
+}
