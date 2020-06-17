a8e241a02e878d1b482bd39a92729643c9465cbe
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-16 18:26:17 -0700

    Add files via upload

---
+++ Exponent.cs
@@ -0,0 +1,28 @@
+﻿// Calculates value based on users base and exponent input.
+
+using System;
+
+namespace Exponent
+{
+	public class Calc
+	{
+		public static double Exponent()
+		{
+			Console.WriteLine("Enter a base value:");
+			double b = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("\nEnter an exponent value:");
+			double e = Convert.ToDouble(Console.ReadLine());
+
+			double result = Math.Pow(b, e);
+			return result;
+		}
+	}
+
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("\n" + Calc.Exponent());
+		}
+	}
+}
