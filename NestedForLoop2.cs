66ca30c4629eeaf4906cb1b173c4ca1755602d31
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-17 12:57:00 -0700

    Add files via upload

---
+++ NestedForLoop2.cs
@@ -0,0 +1,48 @@
+﻿// Outputs a diamond shape of ascending and descending numbers.
+
+using System;
+
+namespace NestedForLoop2
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			int i, j, m, n;
+
+			for (i = 1; i <= 5; i++)
+			{
+				for (j = i; j < 5; j++)
+				{
+					Console.Write(" ");
+				}
+				for (m = 1; m < i; m++)
+				{
+					Console.Write(m);
+				}
+				for (n = i; n > 0; n--)
+				{
+					Console.Write(n);
+				}
+				Console.WriteLine("\n");
+			}
+
+			for (i = 4; i >= 1; i--)
+			{
+				for (j = i; j <= 4; j++)
+				{
+					Console.Write(" ");
+				}
+				for (m = 1; m < i; m++)
+				{
+					Console.Write(m);
+				}
+				for (n = i; n >= 1; n--)
+				{
+					Console.Write(n);
+				}
+				Console.WriteLine("\n");
+			}
+		}
+	}
+}
