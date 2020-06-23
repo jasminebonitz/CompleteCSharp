b7818ffdb3a516e83ded17a4c7c37fe884751067
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-22 19:14:43 -0700

    Add files via upload
    "Write a program to explain protected access specifier."

---
+++ ProtectedAccessModifier.cs
@@ -0,0 +1,34 @@
+﻿// Displays usage of the protected access specifier.
+
+using System;
+
+namespace ProtectedAccessSpecifier
+{
+	public class TestShape
+	{
+		protected string shape;
+	}
+
+	public class TestColor
+	{
+		protected string color;
+		public void Print()
+		{
+			Console.WriteLine("\nMy favorite color is {0}.", color);
+		}
+	}
+
+	public class Program : TestColor
+	{
+		public static void Main()
+		{
+			var p = new Program();
+			Console.WriteLine("Enter your favorite color:");
+			p.color = Console.ReadLine();
+			p.Print();
+
+			var h = new TestShape();
+			// Console.WriteLine(h.shape); creates an error because of the protection level
+		}
+	}
+}
