755fc16f099b3a42acd6fb2a6541123d23130b00
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-24 21:30:52 -0700

    Add files via upload
    "Write a program that calculate add, subtract, multiply and division. Accept two number from the users and then using appropriate function calculate them and shows output to the user."

---
+++ StaticMethods.cs
@@ -0,0 +1,51 @@
+﻿// Displays various math calculations using static methods.
+
+using System;
+
+namespace StaticMethods
+{
+	public class Calculate
+	{
+		static int num1, num2, result;
+		public static void AcceptNums()
+		{
+			Console.WriteLine("Enter first number:");
+			num1 = Int32.Parse(Console.ReadLine());
+			Console.WriteLine("\nEnter second number:");
+			num2 = Int32.Parse(Console.ReadLine());
+		}
+
+		public static void Add()
+		{
+			result = num1 + num2;
+			Console.WriteLine("\n{0} + {1} = {2}", num1, num2, result);
+		}
+		public static void Subtract()
+		{
+			result = num1 - num2;
+			Console.WriteLine("{0} - {1} = {2}", num1, num2, result);
+		}
+		public static void Multiply()
+		{
+			result = num1 * num2;
+			Console.WriteLine("{0} * {1} = {2}", num1, num2, result);
+		}
+		public static void Divide()
+		{
+			result = num1 / num2;
+			Console.WriteLine("{0} / {1} = {2}", num1, num2, result);
+		}
+	}
+
+	public class Program
+	{
+		public static void Main()
+		{
+			Calculate.AcceptNums();
+			Calculate.Add();
+			Calculate.Subtract();
+			Calculate.Multiply();
+			Calculate.Divide();
+		}
+	}
+}
