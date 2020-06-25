3a507b8fea49c491e4c3be0849b0ff59ff11f193
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-24 20:19:49 -0700

    Add files via upload
    "Write a program to store and retrieve value from private variable using get set property."

---
+++ GetSetProperty.cs
@@ -0,0 +1,30 @@
+﻿// Displays usage of the get/set property.
+
+using System;
+
+namespace GetSetProperty
+{
+	public class Animal
+	{
+		private string sound;
+		public string Sound
+		{
+			get { return sound; }
+			set { sound = value; }
+		}
+	}
+
+	public class Program
+	{
+		public static void Main()
+		{
+			var cat = new Animal();
+			cat.Sound = "meow";
+			Console.WriteLine("The cat says " + cat.Sound + "!");
+
+			var dog = new Animal();
+			dog.Sound = "woof";
+			Console.WriteLine("The dog says " + dog.Sound + "!");
+		}
+	}
+}
