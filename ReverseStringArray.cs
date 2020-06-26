8ada5c5d22f3ea3a310e5897b337307963666917
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-25 17:53:55 -0700

    Add files via upload
    "Reverse a string using array."

---
+++ ReverseStringArray.cs
@@ -0,0 +1,20 @@
+﻿// Takes user inputted string and outputs it in reverse without using Array.Reverse.
+
+using System;
+
+namespace ReverseStringArray
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Enter a word or sentence:");
+			string str = Console.ReadLine();
+			char[] ch = str.ToCharArray();
+			for (int i = str.Length - 1; i >= 0; i--)
+			{
+				Console.Write(ch[i]);
+			}
+		}
+	}
+}
