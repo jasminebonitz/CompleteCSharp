7deb0d21aa06656304a0023e145cd5be5c805154
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-06-26 23:33:32 -0700

    Add files via upload
    "Write a constructor program in which you make 3 constructors. One is for default
    constructors with default message, next is parameterized constructor which accepts
    a string value and last one is also parameterized constructor which accept two
    numerical value and shows add of them. Initialize all constructors and shows output."

---
+++ OverloadedConstructors.cs
@@ -0,0 +1,34 @@
+﻿// Demonstrates the usage of a default constructor with 2 overloaded constructors.
+
+using System;
+
+namespace OverloadedConstructors
+{
+	public class Test
+	{
+		public Test()
+		{
+			Console.WriteLine("Default constructor message.");
+		}
+		public Test(string str)
+		{
+			Console.Write("\nFirst overloaded constructor says : ");
+			Console.WriteLine(str);
+		}
+		public Test(int num1, int num2)
+		{
+			int result = num1 + num2;
+			Console.WriteLine("\nSecond overloaded constructor : {0} + {1} = {2}", num1, num2, result);
+		}
+	}
+
+	public class Program
+	{
+		public static void Main()
+		{
+			var test1 = new Test();
+			var test2 = new Test("Was für ne einfache Übung!");
+			var test3 = new Test(23, 77);
+		}
+	}
+}
