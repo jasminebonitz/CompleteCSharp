0025510067ddec2d2da551458de6510786be49c0
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-07-05 23:04:00 -0700

    Add files via upload
    "You are assigned to develop a project and the project manager wants following functionality:
    - Create Student Folder in D drive using the DirectoryInfo class.
    - Ask student's name and create a file with that name and store in Student folder.
    - Ask student's details and save information in that file.
    - Print following option on console screen.
    - View Saved File
    - View Directory Details"

---
+++ FileHandling.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandling
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string path = @"C:\Student\";
+			string studentName, studentLocation, fieldOfStudy;
+			int studentAge;
+
+			DirectoryInfo dir = new DirectoryInfo(path);
+			dir.Create();
+			Console.WriteLine(path + "directory successfully created.");
+
+			Console.WriteLine("\nEnter student name:");
+			studentName = Console.ReadLine();
+			string newPath = path + studentName + ".txt";
+			using (FileStream studentFile = new FileStream(newPath, FileMode.Create))
+			{
+				Console.WriteLine("\n" + studentName + " file is created at " + newPath);
+			}
+
+			Console.WriteLine("\nEnter student age:");
+			studentAge = Int32.Parse(Console.ReadLine());
+			Console.WriteLine("\nEnter student location:");
+			studentLocation = Console.ReadLine();
+			Console.WriteLine("\nEnter field of study:");
+			fieldOfStudy = Console.ReadLine();
+
+			using (StreamWriter addDetails = new StreamWriter(newPath))
+			{
+				addDetails.WriteLine("\nStudent name : " + studentName);
+				addDetails.WriteLine("Age : " + studentAge);
+				addDetails.WriteLine("Location : " + studentLocation);
+				addDetails.WriteLine("Field of Study : " + fieldOfStudy);
+			}
+			Console.WriteLine("\nStudent info saved on " + newPath);
+
+			Start:
+				Console.WriteLine("\nSelect 1 to view saved file, 2 to view directory details, or any other key to exit.");
+				char choice = Console.ReadKey(true).KeyChar;
+				switch (choice)
+				{
+					case '1':
+						{
+							string data;
+							FileStream fsSource = new FileStream(newPath, FileMode.Open, FileAccess.Read);
+							using (StreamReader sr = new StreamReader(fsSource))
+							{
+								data = sr.ReadToEnd();
+							}
+							Console.WriteLine(data);
+							goto Start;
+						}
+					case '2':
+						{
+							Console.WriteLine("\nDirectory name : " + dir.Name);
+							Console.WriteLine("Path : " + dir.FullName);
+							Console.WriteLine("Created on : " + dir.CreationTime);
+							Console.WriteLine("Last accessed : " + dir.LastAccessTime);
+							goto Start;
+						}
+					default:
+						{
+							break;
+						}
+				}
+		}
+	}
+}
