3aad1805d73de1597e9195eb8efb5b07424f8018
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-08-05 19:34:52 -0700

    Add files via upload
    "Write a program for Online Attendance. The conditions are as follows:
    - User provides their name as Input and then application show message to "Welcome to their Name".
    - Jack, Steven and Mathew are banned for the organization. So, when any user enters Jack, Steven and
    Mathew as user name, the application raised an event and fire alarm as well as sends email to administration."

---
+++ EventHandling.cs
@@ -0,0 +1,58 @@
+﻿// Raises an event when certain names are inputted.
+
+using System;
+
+namespace EventHandling
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			UserName uname = new UserName();
+			uname.ev_BannedUser += WarningAlarm;
+			uname.User();
+		}
+
+		static void WarningAlarm(object sender, EventArgs e)
+		{
+			Console.WriteLine("\nBanned user attempting to log on. Sending E-Mail to administration.");
+			Console.WriteLine("Email sent.");
+			Console.WriteLine("Warning alarm initiated...");
+			Console.WriteLine("Press Ctrl + c to stop the alarm");
+			for (; ; )
+			{
+				Console.Beep();
+				System.Threading.Thread.Sleep(100);
+			}
+		}
+	}
+
+	public class UserName
+	{
+		public event EventHandler<EventArgs> ev_BannedUser;
+
+		public void User()
+		{
+			Console.WriteLine("Please enter your name:");
+			string name = Console.ReadLine();
+
+			if (name == "Jack" || name == "Steven" || name == "Mathew")
+			{
+				ev_BannedUser(this, new EventArgs(name));
+			}
+			else
+			{
+				Console.WriteLine("\nWelcome, {0}.", name);
+			}
+		}
+	}
+
+	public class EventArgs
+	{
+		public EventArgs(string s)
+		{
+			Name = s;
+		}
+		public string Name { get; set; }
+	}
+}
