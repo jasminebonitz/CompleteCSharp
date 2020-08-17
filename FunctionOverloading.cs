2f20eb50201fb3068008a49ae85418cc4e59c3f9
jasminebonitz <62082565+jasminebonitz@users.noreply.github.com>
2020-08-17 00:22:50 -0700

    Add files via upload
    "Write a program of function overloading that has a function Convert()
    that accepts a value in parameter and convert them into string and print on screen"

---
+++ FunctionOverloading.cs
@@ -0,0 +1,29 @@
+﻿// Demonstrates function overloadingb by using the same method with different parameters.
+
+using System;
+
+namespace FunctionOverloading
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Program test = new Program();
+			test.Convert(int.MaxValue);
+			test.Convert(double.MaxValue);
+			test.Convert(float.MaxValue);
+		}
+		public void Convert(int num)
+		{
+			Console.WriteLine(num.ToString());
+		}
+		public void Convert(double num)
+		{
+			Console.WriteLine(num.ToString());
+		}
+		public void Convert(float num)
+		{
+			Console.WriteLine(num.ToString());
+		}
+	}
+}
